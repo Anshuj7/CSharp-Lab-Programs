89e89c0754b0f149ffb305eb4a72f8e20f639539
Anshuj7 <48627609+Anshuj7@users.noreply.github.com>
2022-02-14 00:23:28 +0530

    Add files via upload

---
+++ Rel_Log_OP/Rel_Log_OP/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class RelLogOperator
+{
+    public static void Main(string[] args)
+    {
+		bool result;
+		Console.WriteLine("Relational & Logical Operators");
+		Console.WriteLine("\nEnter First Number: ");
+		int fN = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Enter Second Number: ");
+		int sN = Convert.ToInt32(Console.ReadLine());
+
+		result = (fN == sN);
+		Console.WriteLine("{0} == {1} returns {2}", fN, sN, result);
+
+		result = (fN > sN);
+		Console.WriteLine("{0} > {1} returns {2}", fN, sN, result);
+
+		result = (fN < sN);
+		Console.WriteLine("{0} < {1} returns {2}", fN, sN, result);
+
+		result = (fN >= sN);
+		Console.WriteLine("{0} >= {1} returns {2}", fN, sN, result);
+
+		result = (fN <= sN);
+		Console.WriteLine("{0} <= {1} returns {2}", fN, sN, result);
+
+		result = (fN != sN);
+		Console.WriteLine("{0} != {1} returns {2}", fN, sN, result);
+
+		// OR operator
+		result = (fN == sN) || (fN > 5);
+		Console.WriteLine(result);
+
+		// AND operator
+		result = (fN == sN) && (fN > 5);
+		Console.WriteLine(result);
+	}
+}
