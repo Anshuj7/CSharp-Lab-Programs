c9a8cdf365b9fe6393f48ac247f2ba28651d30fb
Anshuj7 <48627609+Anshuj7@users.noreply.github.com>
2022-02-14 00:24:08 +0530

    Add files via upload

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+	class ArithmeticOperator
+	{
+		public static void Main(string[] args)
+		{
+			int result;
+
+			Console.WriteLine("Arithmetic Operation: ");
+			Console.WriteLine("Enter First Number: ");
+			int n1 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Enter Second Number: ");
+			int n2 = Convert.ToInt32(Console.ReadLine());
+
+			result = n1 + n2;
+			Console.WriteLine("Addtition of {0} + {1} = {2}", n1, n2, result);
+
+			result = n1 - n2;
+			Console.WriteLine("Subtraction of {0} - {1} = {2}", n1, n2, result);
+
+			result = n1 * n2;
+			Console.WriteLine("Multiplication of {0} * {1} = {2}", n1, n2, result);
+
+			result = n1 / n2;
+			Console.WriteLine("Division of {0} / {1} = {2}", n1, n2, result);
+
+			result = n1 % n2;
+			Console.WriteLine("Modulo of {0} % {1} = {2}", n1, n2, result);
+
+			result = --n1;
+			Console.WriteLine("Decrement = " + result);
+
+			result = ++n1;
+			Console.WriteLine("Increment = " + result);
+
+		}
+	}
