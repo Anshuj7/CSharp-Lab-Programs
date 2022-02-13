c70559878f2c65344a2448bb92f3bd3ceb0816bc
Anshuj7 <48627609+Anshuj7@users.noreply.github.com>
2022-02-14 00:17:26 +0530

    Add files via upload

---
+++ Max3Num/Max3Num/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Max
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Number 1: ");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Number 2: ");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Number 3: ");
+        int n3 = Convert.ToInt32(Console.ReadLine());
+
+        if (n1 >= n2 && n1 >= n3)
+            Console.WriteLine(n1 + " is the largest number.");
+
+        else if (n2 >= n1 && n2 >= n3)
+            Console.WriteLine(n2 + " is the largest number.");
+
+        else
+            Console.WriteLine(n3 + " is the largest number.");
+    }
+}
