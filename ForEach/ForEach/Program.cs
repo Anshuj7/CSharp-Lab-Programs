cc46fdeaa9fe3cfcda46f4df1f0b993087615efe
Anshuj7 <48627609+Anshuj7@users.noreply.github.com>
2022-02-14 00:14:17 +0530

    Add files via upload

---
+++ ForEach/ForEach/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class ForEach
+{
+    public static void Main()
+    {
+        string[] str = { "Bachelor's", "in", "Computer", "Applications" };
+        foreach (string s in str)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
