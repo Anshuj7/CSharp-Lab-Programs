f58d31fdc37770da45b288115c9e9ba5ba3e9d0b
Anshuj7 <48627609+Anshuj7@users.noreply.github.com>
2022-02-14 00:15:00 +0530

    Add files via upload

---
+++ ForPyra/ForPyra/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class ForPyra
+{
+    public static void Main(string[] args)
+    {
+        int row, sp, col, revcol;
+        Console.WriteLine("Enter the no. of rows for pyramid: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (row = 1; row <= n; row++)
+        {
+            for (sp = 1; sp <= n - row; sp++)
+            {
+                Console.Write(' ');
+            }
+            for (col = 1; col <= row; col++)
+            {
+                Console.Write(col);
+            }
+            for (revcol = col - 2; revcol >= 1; revcol--)
+            {
+                Console.Write(revcol);
+            }
+            Console.WriteLine();
+        }
+    }
+}
