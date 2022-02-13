84c7e7f55e1f8d3c9a18d5b032a58b912341d85b
Anshuj7 <48627609+Anshuj7@users.noreply.github.com>
2022-02-14 00:24:46 +0530

    Add files via upload

---
+++ BxUnBx/BxUnBx/Program.cs
@@ -0,0 +1,24 @@
+﻿// C# implementation to demonstrate
+// the Boxing
+using System;
+class BxUnBx
+{
+
+    // Main Method
+    static public void Main()
+    {
+        int num = 2022;
+
+        object obj = num; //boxing
+        num = 100;
+        int i = (int)obj; //unboxing
+
+        System.Console.WriteLine
+        ("Value - type value of num is : {0}", num);
+        System.Console.WriteLine
+        ("Object - type value of obj is : {0}", obj);
+        
+        System.Console.WriteLine
+        ("Value - Unboxed value of obj is: {0}", obj);
+    }
+}
