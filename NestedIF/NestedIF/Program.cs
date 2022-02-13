ac583c72f64fbeb60f2de2aabb41c6e675d23cc3
Anshuj7 <48627609+Anshuj7@users.noreply.github.com>
2022-02-14 00:16:42 +0530

    Add files via upload

---
+++ NestedIF/NestedIF/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 188, b = 973;
+            if (a == 188)
+            {
+
+                if (b == 973)
+                {
+                    Console.WriteLine("Value of a is 188 and b is 973");
+                }
+            }
+            Console.WriteLine("Exact value of a is : {0}", a);
+            Console.WriteLine("Exact value of b is : {0}", b);
+            Console.ReadLine();
+        }
+    }
