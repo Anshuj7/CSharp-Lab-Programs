02e8d570badbf6d9ae7d520c02f340772f66a96c
Anshuj7 <48627609+Anshuj7@users.noreply.github.com>
2022-02-14 00:12:13 +0530

    Add files via upload

---
+++ MulTab/MulTab/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class DoWhileLoop
+{
+	public static void Main(string[] args)
+	{
+		int i = 1, product;
+		Console.WriteLine("Enter a number: ");
+		int n = int.Parse(Console.ReadLine());
+		do
+		{
+			product = n * i;
+			Console.WriteLine("{0} * {1} = {2}", n, i, product);
+			i++;
+		} while (i <= 10);
+	}
+}
---
+++ fibo/fibo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Fibo
+{
+    public static void Main(string[] args)
+    {
+        int num1 = 0, num2 = 1, num3, num4, num, counter=3;
+        Console.Write("Enter a number: "); 
+        num = int.Parse(Console.ReadLine());
+        Console.Write(num1 + " " + num2);
+        while (counter <= num)
+        {
+            num3 = num1 + num2;
+            if (counter >= num)
+                break;
+            Console.Write(" " + num3);
+            num1 = num2;
+            num2 = num3;
+            counter++;
+            }
+        }
+}
