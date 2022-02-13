f999ae2a6b161e6d1c6e3b5fd97a7d1175e521b5
Anshuj7 <48627609+Anshuj7@users.noreply.github.com>
2022-02-14 00:21:53 +0530

    Add files via upload

---
+++ 1DARR/1DARR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Array1D
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = { 10, 20, 30, 40, 50 };//Declaration and Initialization of array  
+
+        Console.WriteLine("Entered Array is as: ");
+        //traversing array  
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + "\t");
+        }
+    }
+}
